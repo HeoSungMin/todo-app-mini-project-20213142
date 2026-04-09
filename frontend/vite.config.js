b8fcbfb4be10c1cd8de93e0dcc914d059ccbfc29
HeoSungMin <heosung0909@naver.com>
2026-04-10 01:27:55 +0900

    pushshh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // 반드시 dist여야 vercel.json과 맞물립니다.
+  }
 })
